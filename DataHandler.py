b323661d6816570629d3c51b63b7da1148d166c6
leo19900723 <leo19900723@gmail.com>
2020-10-29 22:56:38 -0700

    [add] bar chart for cols choosing [add] PCA

--- DataHandler.py
+++ DataHandler.py
@@ -1,17 +1,23 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 
 class DataHandler(object):
 
-    def __init__(self, dataframe):
-        self._dataframe_original = dataframe
-        self._dataframe_last_update = None
+    def __init__(self, data_frame):
+        self._data_frame_original = data_frame
+        self._data_frame_last_update = None
         self.preprocess_data()
+
+        self._txt_cols = ["attacktype1_txt", "gname"]
+        self._numeric_cols = ["nperps", "nperpcap", "nkill", "nkillus", "nkillter", "nwound", "nwoundus", "nwoundte",
+                              "propvalue", "nhostkid", "nhostkidus", "nhours", "ndays", "nreleased"]
 
     @classmethod
     def construct_from_csv(cls, path):
-        return DataHandler(dataframe=pd.read_csv(path, encoding="ISO-8859-1"))
+        return DataHandler(data_frame=pd.read_csv(path, encoding="ISO-8859-1"))
 
     def preprocess_data(self):
         clean_list = [
@@ -20,35 +26,65 @@ class DataHandler(object):
             "latitude",
             "longitude",
             "targtype1_txt",
-            "weaptype1_txt"
+            "weaptype1_txt",
+            "attacktype1_txt"
         ]
 
         # Clean data
-        self._dataframe_last_update = self._dataframe_original.replace(to_replace="Unknown", value=np.nan)
-        self._dataframe_last_update = self._dataframe_last_update.dropna(subset=clean_list)
+        self._data_frame_last_update = self._data_frame_original.replace(to_replace="Unknown", value=np.nan)
+        self._data_frame_last_update = self._data_frame_last_update.dropna(subset=clean_list)
+        self._data_frame_last_update = self._data_frame_last_update.reset_index()
 
-        # Apply changes to original dataframe
+        # Apply changes to original data_frame
         self.apply_last_update()
 
+    def get_data_frame_pca(self, tag):
+
+        self._data_frame_last_update = self._data_frame_original[self._numeric_cols]
+        self._data_frame_last_update = self._data_frame_last_update.apply(lambda x: x.fillna(0))
+        self._data_frame_last_update = pd.DataFrame(StandardScaler().fit_transform(self._data_frame_last_update),
+                                                     columns=self._numeric_cols)
+
+        pca = PCA(n_components=2)
+        df_pca = pca.fit_transform(self._data_frame_last_update)
+        df_pca = pd.DataFrame(data=df_pca, columns=["x", "y"])
+        df_pca[tag] = self._data_frame_original[tag]
+
+        return df_pca
+
     def apply_last_update(self):
-        self._dataframe_original = self._dataframe_last_update.copy()
+        self._data_frame_original = self._data_frame_last_update.copy()
 
     def reset_last_update(self):
-        self._dataframe_last_update = self._dataframe_original.copy()
+        self._data_frame_last_update = self._data_frame_original.copy()
+
+    @staticmethod
+    def trim_categories(data_frame, target_col, number_of_reserved=8):
+        df = data_frame.copy()
+
+        if len(data_frame[target_col].unique()) >= number_of_reserved:
+            reserved_categories = data_frame[target_col].value_counts().head(number_of_reserved).index
+            df.loc[~data_frame[target_col].isin(reserved_categories), target_col] = "Others"
+
+        return df
 
     @property
-    def get_dataframe_original(self):
-        return self._dataframe_original
+    def get_data_frame_original(self):
+        return self._data_frame_original
 
     @property
-    def get_dataframe_last_update(self):
-        return self._dataframe_last_update
+    def get_data_frame_last_update(self):
+        return self._data_frame_last_update
+
+    @property
+    def get_txt_categories(self):
+        return self._txt_cols
 
 
 def unit_test():
     path = "dataset/globalterrorismdb_0718dist.csv"
     handler = DataHandler.construct_from_csv(path=path)
-    print(handler.get_dataframe_original)
+    print(DataHandler.trim_categories(handler.get_data_frame_original, "gname"))
 
 
 if __name__ == '__main__':
--- DataVisualizer.py
+++ DataVisualizer.py
@@ -14,8 +14,9 @@ class DataVisualizer(object):
         self._data_handler = data_handler
 
         self._default_year_range = [2000, 2017]
-        self._default_group_list = self._data_handler.get_dataframe_original["gname"].value_counts().head(8).index
-        self._default_figure_template = "ggplot2"
+        self._default_group_list = self._data_handler.get_data_frame_original["gname"].value_counts().head(8).index
+        self._default_bar_pick = self._data_handler.get_txt_categories[0]
+        self._default_pca_pick = "region_txt"
         self._default_legend_style = {
             "orientation": "h",
             "yanchor": "bottom",
@@ -45,10 +46,10 @@ class DataVisualizer(object):
                     children=[
                         dcc.RangeSlider(
                             id="year_slider",
-                            min=self._data_handler.get_dataframe_original["iyear"].min(),
-                            max=self._data_handler.get_dataframe_original["iyear"].max(),
+                            min=self._data_handler.get_data_frame_original["iyear"].min(),
+                            max=self._data_handler.get_data_frame_original["iyear"].max(),
                             marks={str(year): str(year) for year in
-                                   self._data_handler.get_dataframe_original["iyear"].unique()},
+                                   self._data_handler.get_data_frame_original["iyear"].unique()},
                             value=self._default_year_range
                         ),
                         html.Br(),
@@ -61,32 +62,62 @@ class DataVisualizer(object):
                 html.Div(
                     id="row10",
                     children=[
-                        html.H5(children="Control Panel"),
-                        dcc.Dropdown(
-                            id="group_selector",
-                            options=[{"label": group, "value": group} for group in
-                                     self._data_handler.get_dataframe_original["gname"].unique()],
-                            value=self._default_group_list,
-                            multi=True
-                        )
+                        html.Label([
+                            "Group Selector",
+                            dcc.Dropdown(
+                                id="group_selector",
+                                options=[{"label": group, "value": group} for group in
+                                         self._data_handler.get_data_frame_original["gname"].unique()],
+                                value=self._default_group_list,
+                                multi=True
+                            )
+                        ]),
+                        html.Label([
+                            "Bar Chart Category View Picker",
+                            dcc.Dropdown(
+                                id="bar_chart_view_picker",
+                                options=[{"label": col, "value": col} for col in self._data_handler.get_txt_categories],
+                                value=self._default_bar_pick
+                            )
+                        ]),
+                        html.Label([
+                            "PCA Category View Picker",
+                            dcc.Dropdown(
+                                id="pca_view_picker",
+                                options=[{"label": col, "value": col} for col in
+                                         self._data_handler.get_data_frame_original.columns],
+                                value=self._default_pca_pick
+                            )
+                        ])
                     ],
                     className="block_style"
                 ),
                 html.Div(
                     id="row11",
                     children=[
-                        html.H5(children="Attacks Map by Groups"),
-                        dcc.Graph(id="map_year_attack_group_fig")
+                        html.H5(children="Yearly Accumulated Attacks and Types"),
+                        dcc.Graph(id="bar_year_attack_type_all_fig")
                     ],
                     className="block_style"
                 ),
             ]),
 
+            html.Div(id="row2", children=[
+                html.Div(
+                    id="row20",
+                    children=[
+                        html.H5(children="Attacks Map by Groups"),
+                        dcc.Graph(id="map_year_attack_group_fig")
+                    ],
+                    className="block_style"
+                )
+            ]),
+
             html.Div(
-                id="row2",
+                id="row3",
                 children=[
                     html.Div(
-                        id="row20",
+                        id="row30",
                         children=[
                             html.H5(children="Yearly Attacks by Groups"),
                             dcc.Graph(id="line_year_attack_group_fig")
@@ -94,14 +125,27 @@ class DataVisualizer(object):
                         className="block_style"
                     ),
                     html.Div(
-                        id="row21",
+                        id="row31",
                         children=[
                             html.H5(children="Target Type by Selected Attacks"),
                             dcc.Graph(id="pie_year_target_selected_group_fig")
                         ],
                         className="block_style"
                     )
-                ])
+                ]),
+            html.Div(
+                id="row4",
+                children=[
+                    html.Div(
+                        id="row40",
+                        children=[
+                            html.H5(children="PCA"),
+                            dcc.Graph(id="pca")
+                        ],
+                        className="block_style"
+                    )
+                ]
+            )
         ])
 
         @self._app.callback(
@@ -109,7 +153,7 @@ class DataVisualizer(object):
             [dash.dependencies.Input("year_slider", "value"),
              dash.dependencies.Input("group_selector", "value")])
         def update_map_year_attack_group_fig(year_range, selected_group):
-            df = self._data_handler.get_dataframe_original
+            df = self._data_handler.get_data_frame_original
             df = df[df["iyear"].between(year_range[0], year_range[1], inclusive=True)]
             df = df[df["gname"].isin(selected_group)]
             df = df.groupby(["gname", "latitude", "longitude"]).size().reset_index(name="frequency")
@@ -117,48 +161,72 @@ class DataVisualizer(object):
             fig = px.scatter_mapbox(data_frame=df, lat="latitude", lon="longitude", color="gname", size="frequency",
                                     zoom=2, custom_data=["gname", "latitude", "longitude"],
                                     color_continuous_scale=px.colors.cyclical.IceFire, mapbox_style="open-street-map",
-                                    template=self._default_figure_template)
+                                    template="ggplot2")
+            fig.update_layout(legend=self._default_legend_style)
+            return fig
+
+        @self._app.callback(
+            dash.dependencies.Output("bar_year_attack_type_all_fig", "figure"),
+            [dash.dependencies.Input("year_slider", "value"),
+             dash.dependencies.Input("bar_chart_view_picker", "value")])
+        def update_bar_year_attack_type_all_fig(year_range, selected_col):
+            selected_col = selected_col if selected_col else self._default_bar_pick
+            self._default_bar_pick = selected_col
+
+            df = self._data_handler.get_data_frame_original
+            df = DataHandler.trim_categories(data_frame=df, target_col=selected_col)
+            df = df[df["iyear"].between(year_range[0], year_range[1], inclusive=True)]
+            df = df.groupby(["iyear", selected_col]).size().reset_index(name="frequency")
+
+            fig = px.bar(data_frame=df, x="iyear", y="frequency", color=selected_col, template="plotly_white")
             fig.update_layout(legend=self._default_legend_style)
             return fig
 
         @self._app.callback(
             dash.dependencies.Output("line_year_attack_group_fig", "figure"),
-            [dash.dependencies.Input("year_slider", "value"), dash.dependencies.Input("group_selector", "value")])
-        def update_line_year_attack_group_fig(year_range, selected_group):
-            df = self._data_handler.get_dataframe_original
-            df = df[df["iyear"].between(year_range[0], year_range[1], inclusive=True)]
-            df = df[df["gname"].isin(selected_group)]
-            df = df.groupby(["iyear", "gname"]).size().reset_index(name="frequency")
-
-            fig = px.line(data_frame=df, x="iyear", y="frequency", color="gname",
-                          template=self._default_figure_template)
-            fig.update_layout(legend=self._default_legend_style)
-            return fig
-
-        @self._app.callback(
             dash.dependencies.Output("pie_year_target_selected_group_fig", "figure"),
             [dash.dependencies.Input("year_slider", "value"),
              dash.dependencies.Input("map_year_attack_group_fig", "selectedData"),
              dash.dependencies.Input("group_selector", "value")])
-        def update_pie_year_weapon_target_selected_group_fig(year_range, selected_points, selected_group):
-            if selected_points:
+        def update_line_pie_attack_target_selected_group(year_range, selected_points, selected_group):
+            if selected_points and selected_points["points"]:
                 df_selected_points = pd.DataFrame([row["customdata"] for row in selected_points["points"]],
                                                   columns=["gname", "latitude", "longitude"])
             else:
-                df_selected_points = self._data_handler.get_dataframe_original
+                df_selected_points = self._data_handler.get_data_frame_original
                 df_selected_points = df_selected_points[df_selected_points["gname"].isin(selected_group)][
                     ["gname", "latitude", "longitude"]]
 
-            df = self._data_handler.get_dataframe_original
+            df = self._data_handler.get_data_frame_original
             df = df[df["iyear"].between(year_range[0], year_range[1], inclusive=True)]
-            df = df[df["gname"].isin(df_selected_points["gname"])]
+            df = df[df["gname"].isin(selected_group)]
             df = df[df["latitude"].isin(df_selected_points["latitude"])]
             df = df[df["longitude"].isin(df_selected_points["longitude"])]
 
+            df_attack = df.groupby(["iyear", "gname"]).size().reset_index(name="frequency")
             df_target = df.groupby(["targtype1_txt"]).size().reset_index(name="frequency")
 
-            fig = px.pie(data_frame=df_target, values=df_target["frequency"], template=self._default_figure_template)
-            fig.update_layout(showlegend=False)
+            fig_line = px.line(data_frame=df_attack, x="iyear", y="frequency", color="gname", template="plotly_white")
+            fig_line.update_layout(legend=self._default_legend_style)
+
+            fig_pie = px.pie(data_frame=df_target, values="frequency", names="targtype1_txt", template="ggplot2")
+            fig_pie.update_traces(textposition="inside", textinfo="percent+label")
+            fig_pie.update_layout(showlegend=False)
+
+            return fig_line, fig_pie
+
+        @self._app.callback(
+            dash.dependencies.Output("pca", "figure"),
+            [dash.dependencies.Input("pca_view_picker", "value")])
+        def update_pca(picked_col):
+            picked_col = picked_col if picked_col else self._default_pca_pick
+            self._default_pca_pick = picked_col
+
+            df_pca = self._data_handler.get_data_frame_pca(picked_col)
+            df_pca = DataHandler.trim_categories(data_frame=df_pca, target_col=picked_col)
+
+            fig = px.scatter(data_frame=df_pca, x="x", y="y", color=picked_col)
+            fig.update_layout(legend=self._default_legend_style)
             return fig
 
     def run_server(self):
